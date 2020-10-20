17bfc08f0dc70d6083172820b348be5972e3bfd2
daoge <daoge@daoge-win>
2020-10-20 10:21:12 +0800

    1

--- bigdata.cpp
+++ bigdata.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
+#include<string>
+#define _GLIBCXX_USE_CXX11_ABI 1
 using namespace std;
-string input(){
-    string a;
+void input(char *a,int n){
+     
     cout<<"输入二进制整数"<<endl;
     cin>>a;
     return a;
 }
 void cut(string a,string &b,string &c){
     int n=a.length();
-    
-    for(int i=0;i<a.)
+    for(int i=0;i<n/2;i++)
+        b.insert('1');
+    for(int i=0;i<n/2;i++)
+        c[i]=a[i+n/2];
 }
 
 int main(){
-    string a,b,c;
+    string a=new string();
     a=input();
-    b=input();
-    cut(a,a1,a2);
-    
+    a.insert(1,'1');
+//  b=input();
+    cout<<a<<endl;;
+    cout<<a1;
     return 0;
 }
